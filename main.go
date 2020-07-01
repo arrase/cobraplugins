c8877528a66f9352998ba23f6b402da7e15c6ee6
Juan Ezquerro LLanes <juan@syncrtc.com>
2020-07-01 21:07:41 +0200

    First commit

---
+++ main.go
@@ -0,0 +1,20 @@
+package main
+
+import (
+	"plugin"
+  "github.com/spf13/cobra"
+)
+
+func Load(rootCdm *cobra.Command, pluginDir string) {
+	p, err := plugin.Open(pluginDir)
+	if err != nil {
+		panic(err)
+	}
+
+	b, err := p.Lookup("PluginCmd")
+	if err != nil {
+		panic(err)
+	}
+
+  rootCdm.AddCommand(*b.(**cobra.Command))
+}
