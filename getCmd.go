79e3c827ba1a7c8abf6c80f1d2914930757e83a6
Juan Ezquerro LLanes <juan@syncrtc.com>
2020-07-06 00:09:29 +0200

    Get CMD

---
+++ getCmd.go
@@ -0,0 +1,19 @@
+package cobraplugins
+
+import (
+	"github.com/spf13/cobra"
+	"plugin"
+)
+
+func GetCmd(pluginPath, cmdName string) *cobra.Command {
+	p, err := plugin.Open(pluginPath)
+	if err != nil {
+		panic(err)
+	}
+	b, err := p.Lookup(cmdName)
+	if err != nil {
+		panic(err)
+	}
+
+	retrun *b.(**cobra.Command)
+}
